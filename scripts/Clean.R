fc53cb428f55cad4e9df3ef821da756227611d79
kesiabomfa <52920939+kesiabomfa@users.noreply.github.com>
2020-08-18 11:36:03 -0300

    atualiza script Clean.R

--- scripts/Clean.R
+++ scripts/Clean.R
@@ -4,13 +4,10 @@ library(magrittr);library(jsonlite)
 input <- "data-raw/letters.xlsx"
 output <- jsonlite::read_json("datapackage.json")$resources[[1]]$path
 
-read_json("datapackage.json")
-
 x <- read_json("datapackage.json")
 
 result <- input %>% 
   read() %>% 
-  recode() %>% 
-  enrich()
 
-data.table::fwrite(result, file = output, sep = ";", dec = ",", bom = TRUE, na = "NA")
+
+readr::write_excel_csv(result, path = output)
